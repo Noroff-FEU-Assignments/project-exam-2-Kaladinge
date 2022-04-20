bc2386895e52b8317f350cc6085caf2dbdaeff48
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-04-20 12:47:04 +0200

    made the navigation bar and roughly styled it

--- src/App.js
+++ src/App.js
@@ -15,34 +15,46 @@ import Accommodations from "./components/accommodations/Accommodations";
 import Accommodation from "./components/accommodation/Accommodation";
 import Contact from "./components/contact/Contact";
 import Login from "./components/login/Login";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
 
 function App() {
   return (
     <Router>
-      <Navbar expand="lg" className="p-4 bg-dark navbar-dark">
-        <Container>
-          <NavLink to="/" className="text-decoration-none">
-            <Navbar.Brand>My logo</Navbar.Brand>
-          </NavLink>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-5">
+      <Navbar expand="lg" className="bg-dark navbar-dark position-relative">
+        <NavLink to="/" className="d-lg-none">
+          <Navbar.Brand>mobile</Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <div className="d-flex flex-column flex-lg-row justify-content-around w-100">
+            <NavLink to="/" className="text-decoration-none d-none d-lg-block">
+              <Navbar.Brand>desktop</Navbar.Brand>
+            </NavLink>
+            <Nav className="">
               <NavLink to="/accommodations" className="nav-link">
                 Accommodations
               </NavLink>
             </Nav>
-            <Nav className="mx-5">
+            <Form action="/action_page.php" className="w-50">
+              <FormControl
+                type="text"
+                placeholder="Search accommodation"
+                name="search"
+              />
+            </Form>
+            <Nav className="">
               <NavLink to="/contact" className="nav-link">
                 Contact
               </NavLink>
             </Nav>
-            <Nav className="mx-5">
+            <Nav className="">
               <NavLink to="/login" className="nav-link">
                 Login
               </NavLink>
             </Nav>
-          </Navbar.Collapse>
-        </Container>
+          </div>
+        </Navbar.Collapse>
       </Navbar>
       <Container>
         <Routes>
