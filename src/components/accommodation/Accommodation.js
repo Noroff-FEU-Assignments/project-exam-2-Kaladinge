15ed0a4876a93f6a1c88ebf8f39fa5aacb8a11a3
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-04-25 16:11:22 +0200

    Added useState constants to the detail page

--- src/components/accommodation/Accommodation.js
+++ src/components/accommodation/Accommodation.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Accommodation() {
+  const [accommodation, setAccommodation] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [fetchDataError, setFetchDataError] = useState(null);
+
   return <div>Accommodation</div>;
 }
 
