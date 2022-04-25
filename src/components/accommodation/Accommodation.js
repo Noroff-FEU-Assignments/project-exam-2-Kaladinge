062376d226a9716054d1ab1113f15be7e98d7191
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-04-25 17:00:54 +0200

    added usestate constants to the form

--- src/components/accommodation/Accommodation.js
+++ src/components/accommodation/Accommodation.js
@@ -1,9 +1,37 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 function Accommodation() {
   const [accommodation, setAccommodation] = useState([]);
   const [loading, setLoading] = useState(true);
   const [fetchDataError, setFetchDataError] = useState(null);
+
+  const { id } = useParams("/");
+  const url = `https://kaladinge-pe2.herokuapp.com/api/accommodations/${id}?populate=*`;
+
+  useEffect(() => {
+    const getAccommodation = async () => {
+      try {
+        const response = await axios.get(url);
+        console.log(response.data.data);
+        setAccommodation(response.data.data);
+      } catch (error) {
+        setFetchDataError(error.toString());
+      } finally {
+        setLoading(false);
+      }
+    };
+    getAccommodation();
+  }, [url]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (fetchDataError) {
+    return <div>There was a fetch data error</div>;
+  }
 
   return <div>Accommodation</div>;
 }
