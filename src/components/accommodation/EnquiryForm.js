bb102c79fc03dda59298a046207564ec302bb06f
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-04-26 11:25:27 +0200

    added simple modal functionality

--- src/components/accommodation/EnquiryForm.js
+++ src/components/accommodation/EnquiryForm.js
@@ -1,12 +1,26 @@
 import { Button } from "react-bootstrap";
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
 function EnquiryForm() {
+  const [displayMode, setDisplayMode] = useState(false);
+
+  function showForm() {
+    console.log(displayMode);
+    setDisplayMode(!displayMode);
+  }
+
   return (
-    <Form className="bg-light p-3 d-flex flex-column mx-auto">
-      <Button>Check availability</Button>
-    </Form>
+    <>
+      <Button onClick={showForm}>Check availability</Button>
+      <Form
+        className={`bg-light p-3 d-flex flex-column mx-auto ${
+          displayMode === false ? "d-none" : "d-block"
+        }`}
+      >
+        <div>modal</div>
+      </Form>
+    </>
   );
 }
 
