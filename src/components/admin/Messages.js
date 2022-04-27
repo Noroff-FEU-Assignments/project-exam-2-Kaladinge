277eea428617f364184f6a7074572d165013ac68
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-04-27 16:18:03 +0200

    added table data to the component

--- src/components/admin/Messages.js
+++ src/components/admin/Messages.js
@@ -1,27 +1,27 @@
 import React, { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
 import Heading from "../layout/Heading";
 
 function Messages() {
-  const [accommodations, setAccommodations] = useState([]);
+  const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(true);
   const [fetchPagesError, setFetchPagesError] = useState(null);
 
   const url = "https://kaladinge-pe2.herokuapp.com/api/messages";
 
   useEffect(() => {
-    const getAccommodations = async () => {
+    const getMessages = async () => {
       try {
         const response = await fetch(url);
         const result = await response.json();
-        console.log(result.data);
-        setAccommodations(result.data);
+        setMessages(result.data);
       } catch (error) {
         setFetchPagesError(error.toString());
       } finally {
         setLoading(false);
       }
     };
-    getAccommodations();
+    getMessages();
   }, [url]);
 
   if (loading) {
@@ -29,10 +29,39 @@ function Messages() {
   }
 
   if (fetchPagesError) {
-    return <div>There was a fetch pages error</div>;
+    return <div>There was a fetch messages error</div>;
   }
 
-  return <Heading title="Messages" />;
+  return (
+    <>
+      <div className="d-flex">
+        <Heading title="Messages" />
+        <p>{messages.length}</p>
+      </div>
+      <Table striped bordered hover responsive="md">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Email</th>
+            <th>Subject</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {messages.map((item, index) => {
+            return (
+              <tr key={item.id}>
+                <td>{index + 1}</td>
+                <td>{item.attributes.email}</td>
+                <td>{item.attributes.subject}</td>
+                <td>{item.attributes.message}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </>
+  );
 }
 
 export default Messages;
