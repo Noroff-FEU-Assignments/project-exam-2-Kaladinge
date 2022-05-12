848f6e105f499bef95bbb1fec6704f411bb84229
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-05-12 13:54:09 +0200

    added thumbnail images

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { categoryImages } from "../../constants/data";
 
 function Home() {
+  console.log(categoryImages);
   return (
-    <div className="hero-banner mb-5">
-      <h2 className="hero-banner--text text-center">Bergen welcomes you!</h2>
-    </div>
+    <>
+      <div className="hero-banner mb-5">
+        <h2 className="hero-banner--text text-center">Bergen welcomes you!</h2>
+      </div>
+      <div className="categories-thumbnails d-md-flex justify-content-between">
+        {categoryImages.map((item) => (
+          <div
+            key={item.text}
+            className="categories-thumbnails--button mx-end mb-3 position-relative border"
+          >
+            <img
+              src={item.image}
+              className="categories-thumbnails--image w-100"
+            />
+            <h3 className="categories-thumbnails--text position-absolute bottom-0 p-4 m-0 bg-secondary w-100 text-white text-center">
+              {item.text}
+            </h3>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
---
+++ src/constants/data.js
@@ -0,0 +1,18 @@
+import hotelThumbnail from "../images/thumbnail-hotel.jpg";
+import BnbThumbnail from "../images/thumbnail-bnb.jpg";
+import guesthouseThumbnail from "../images/thumbnail-guesthouse.jpg";
+
+export const categoryImages = [
+  {
+    image: hotelThumbnail,
+    text: "Hotels",
+  },
+  {
+    image: BnbThumbnail,
+    text: "B & B",
+  },
+  {
+    image: guesthouseThumbnail,
+    text: "Guesthouses",
+  },
+];
